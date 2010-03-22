a7eaa56940e771139b71040c0e7c1932514ba7d8
frank.cornelis <frank.cornelis@c569a5dc-3e0d-11de-97fb-aff7d2a6cc54>
2010-03-22 15:24:29 +0000

    basics for the OpenID protocol handler service

---
+++ eid-idp-webapp/src/main/java/be/fedict/eid/idp/webapp/IdentityProviderServletContextListener.java
@@ -0,0 +1,42 @@
+/*
+ * eID Identity Provider Project.
+ * Copyright (C) 2010 FedICT.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/.
+ */
+
+package be.fedict.eid.idp.webapp;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class IdentityProviderServletContextListener implements
+		ServletContextListener {
+
+	private static final Log LOG = LogFactory
+			.getLog(IdentityProviderServletContextListener.class);
+
+	public void contextInitialized(ServletContextEvent event) {
+		LOG.debug("contextInitialized");
+		ServletContext servletContext = event.getServletContext();
+	}
+
+	public void contextDestroyed(ServletContextEvent event) {
+		LOG.debug("contextDestroy");
+	}
+}
