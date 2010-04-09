a58fc58a13092c54abc2f692e7aa658ea69740d1
frank.cornelis <frank.cornelis@c569a5dc-3e0d-11de-97fb-aff7d2a6cc54>
2010-04-09 14:50:44 +0000

    prefixing the table names

--- eid-idp-model/src/main/java/be/fedict/eid/idp/model/entity/ConfigEntity.java
+++ eid-idp-model/src/main/java/be/fedict/eid/idp/model/entity/ConfigEntity.java
@@ -25,7 +25,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "configuration")
+@Table(name = "idp_configuration")
 public class ConfigEntity implements Serializable {
 
 	private static final long serialVersionUID = 1L;
