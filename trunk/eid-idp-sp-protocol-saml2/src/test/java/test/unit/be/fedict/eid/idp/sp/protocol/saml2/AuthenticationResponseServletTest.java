7e4182b419899955791f6abdea91d687d9b5156e
wim.vandenhaute@gmail.com <wim.vandenhaute@gmail.com@c569a5dc-3e0d-11de-97fb-aff7d2a6cc54>
2010-12-24 11:42:12 +0000

    fix: unit test failing due to log4j/slf4j dependency issue caused by opensaml

--- trunk/eid-idp-sp-protocol-saml2/src/test/java/test/unit/be/fedict/eid/idp/sp/protocol/saml2/AuthenticationResponseServletTest.java
+++ trunk/eid-idp-sp-protocol-saml2/src/test/java/test/unit/be/fedict/eid/idp/sp/protocol/saml2/AuthenticationResponseServletTest.java
@@ -18,16 +18,7 @@
 
 package test.unit.be.fedict.eid.idp.sp.protocol.saml2;
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
+import be.fedict.eid.idp.sp.protocol.saml2.AuthenticationResponseServlet;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpClient;
@@ -46,106 +37,113 @@ import org.mortbay.jetty.servlet.ServletHolder;
 import org.mortbay.jetty.servlet.SessionHandler;
 import org.mortbay.jetty.testing.ServletTester;
 
-import be.fedict.eid.idp.sp.protocol.saml2.AuthenticationResponseServlet;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import static org.junit.Assert.assertEquals;
 
 public class AuthenticationResponseServletTest {
 
-	private static final Log LOG = LogFactory
-			.getLog(AuthenticationResponseServletTest.class);
+    private static final Log LOG = LogFactory
+            .getLog(AuthenticationResponseServletTest.class);
 
-	private ServletTester servletTester;
+    private ServletTester servletTester;
 
-	private String location;
+    private String location;
 
-	@Before
-	public void setUp() throws Exception {
-		this.servletTester = new ServletTester();
-		ServletHolder servletHolder = this.servletTester.addServlet(
-				AuthenticationResponseServlet.class, "/");
-		servletHolder.setInitParameter("IdentifierSessionAttribute",
-				"identifier");
-		servletHolder.setInitParameter("RedirectPage", "target-page");
-		servletHolder.setInitParameter("NameSessionAttribute", "name");
-		this.servletTester.start();
-		this.location = this.servletTester.createSocketConnector(true);
-	}
+    @Before
+    public void setUp() throws Exception {
+        this.servletTester = new ServletTester();
+        ServletHolder servletHolder = this.servletTester.addServlet(
+                AuthenticationResponseServlet.class, "/");
+        servletHolder.setInitParameter("IdentifierSessionAttribute",
+                "identifier");
+        servletHolder.setInitParameter("RedirectPage", "target-page");
+        servletHolder.setInitParameter("NameSessionAttribute", "name");
+        this.servletTester.start();
+        this.location = this.servletTester.createSocketConnector(true);
+    }
 
-	@After
-	public void tearDown() throws Exception {
-		this.servletTester.stop();
-	}
+    @After
+    public void tearDown() throws Exception {
+        this.servletTester.stop();
+    }
 
-	@Test
-	public void testDoGet() throws Exception {
-		// setup
-		LOG.debug("URL: " + this.location);
-		HttpClient httpClient = new HttpClient();
-		GetMethod getMethod = new GetMethod(this.location);
+    @Test
+    public void testDoGet() throws Exception {
+        // setup
+        LOG.debug("URL: " + this.location);
+        HttpClient httpClient = new HttpClient();
+        GetMethod getMethod = new GetMethod(this.location);
 
-		// operate
-		int result = httpClient.executeMethod(getMethod);
+        // operate
+        int result = httpClient.executeMethod(getMethod);
 
-		// verify
-		LOG.debug("result: " + result);
-		assertEquals(HttpServletResponse.SC_METHOD_NOT_ALLOWED, result);
-	}
+        // verify
+        LOG.debug("result: " + result);
+        assertEquals(HttpServletResponse.SC_METHOD_NOT_ALLOWED, result);
+    }
 
-	@Test
-	public void testPostSamlResponse() throws Exception {
-		doPostSamlResponseTest("/saml-response.xml");
-	}
+    @Test
+    public void testPostSamlResponse() throws Exception {
+        doPostSamlResponseTest("/saml-response.xml");
+    }
 
-	//@Test
-	public void testPostSignedSamlResponse() throws Exception {
-		doPostSamlResponseTest("/saml-response-signed.xml");
-	}
+    //@Test
+    public void testPostSignedSamlResponse() throws Exception {
+        doPostSamlResponseTest("/saml-response-signed.xml");
+    }
 
-	private void doPostSamlResponseTest(String samlResponseResourceName)
-			throws IOException {
-		// setup
-		InputStream samlResponseInputStream = AuthenticationResponseServletTest.class
-				.getResourceAsStream(samlResponseResourceName);
-		String samlResponse = IOUtils.toString(samlResponseInputStream);
-		String encodedSamlResponse = Base64.encodeBase64String(samlResponse
-				.getBytes());
+    private void doPostSamlResponseTest(String samlResponseResourceName)
+            throws IOException {
+        // setup
+        InputStream samlResponseInputStream = AuthenticationResponseServletTest.class
+                .getResourceAsStream(samlResponseResourceName);
+        String samlResponse = IOUtils.toString(samlResponseInputStream);
+        String encodedSamlResponse = Base64.encodeBase64String(samlResponse
+                .getBytes());
 
-		LOG.debug("URL: " + this.location);
-		HttpClient httpClient = new HttpClient();
-		PostMethod postMethod = new PostMethod(this.location);
-		NameValuePair[] body = { new NameValuePair("SAMLResponse",
-				encodedSamlResponse) };
-		postMethod.setRequestBody(body);
+        LOG.debug("URL: " + this.location);
+        HttpClient httpClient = new HttpClient();
+        PostMethod postMethod = new PostMethod(this.location);
+        NameValuePair[] body = {new NameValuePair("SAMLResponse",
+                encodedSamlResponse)};
+        postMethod.setRequestBody(body);
 
-		// operate
-		int resultStatusCode = httpClient.executeMethod(postMethod);
+        // operate
+        int resultStatusCode = httpClient.executeMethod(postMethod);
 
-		// verify
-		LOG.debug("result: " + resultStatusCode);
-		assertEquals(HttpServletResponse.SC_MOVED_TEMPORARILY, resultStatusCode);
-		Header locationHeader = postMethod.getResponseHeader("Location");
-		String value = locationHeader.getValue();
-		assertEquals(this.location + "/target-page", value);
+        // verify
+        LOG.debug("result: " + resultStatusCode);
+        assertEquals(HttpServletResponse.SC_MOVED_TEMPORARILY, resultStatusCode);
+        Header locationHeader = postMethod.getResponseHeader("Location");
+        String value = locationHeader.getValue();
+        assertEquals(this.location + "/target-page", value);
 
-		SessionHandler sessionHandler = this.servletTester.getContext()
-				.getSessionHandler();
-		SessionManager sessionManager = sessionHandler.getSessionManager();
-		LOG.debug("session manager type: "
-				+ sessionManager.getClass().getName());
-		HashSessionManager hashSessionManager = (HashSessionManager) sessionManager;
-		LOG.debug("# sessions: " + hashSessionManager.getSessions());
-		assertEquals(1, hashSessionManager.getSessions());
-		Map<String, HttpSession> sessionMap = hashSessionManager
-				.getSessionMap();
-		LOG.debug("session map: " + sessionMap);
-		Entry<String, HttpSession> sessionEntry = sessionMap.entrySet()
-				.iterator().next();
-		HttpSession httpSession = sessionEntry.getValue();
+        SessionHandler sessionHandler = this.servletTester.getContext()
+                .getSessionHandler();
+        SessionManager sessionManager = sessionHandler.getSessionManager();
+        LOG.debug("session manager type: "
+                + sessionManager.getClass().getName());
+        HashSessionManager hashSessionManager = (HashSessionManager) sessionManager;
+        LOG.debug("# sessions: " + hashSessionManager.getSessions());
+        assertEquals(1, hashSessionManager.getSessions());
+        Map<String, HttpSession> sessionMap = hashSessionManager
+                .getSessionMap();
+        LOG.debug("session map: " + sessionMap);
+        Entry<String, HttpSession> sessionEntry = sessionMap.entrySet()
+                .iterator().next();
+        HttpSession httpSession = sessionEntry.getValue();
 
-		String identifierValue = (String) httpSession
-				.getAttribute("identifier");
-		assertEquals("authn-id", identifierValue);
+        String identifierValue = (String) httpSession
+                .getAttribute("identifier");
+        assertEquals("authn-id", identifierValue);
 
-		String nameValue = (String) httpSession.getAttribute("name");
-		assertEquals("test-name", nameValue);
-	}
+        String nameValue = (String) httpSession.getAttribute("name");
+        assertEquals("test-name", nameValue);
+    }
 }
