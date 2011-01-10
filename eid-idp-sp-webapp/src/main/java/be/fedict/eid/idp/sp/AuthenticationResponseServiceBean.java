bdd2c94eac1b2ac03218c20f7366c4109de13d52
wim.vandenhaute <wim.vandenhaute@gmail.com>
2011-01-10 14:03:56 +0000

    Add SAML 2 AuthenticationResponseService for optional validation of cert.chain in response

---
+++ eid-idp-sp-webapp/src/main/java/be/fedict/eid/idp/sp/AuthenticationResponseServiceBean.java
@@ -0,0 +1,20 @@
+package be.fedict.eid.idp.sp;
+
+import be.fedict.eid.idp.sp.protocol.saml2.spi.AuthenticationResponseService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.io.Serializable;
+import java.security.cert.X509Certificate;
+import java.util.List;
+
+public class AuthenticationResponseServiceBean implements AuthenticationResponseService, Serializable {
+
+    private static final Log LOG = LogFactory.getLog(AuthenticationResponseServiceBean.class);
+
+    @Override
+    public void validateServiceCertificate(List<X509Certificate> certificateChain) throws SecurityException {
+
+        LOG.debug("validate saml response cert.chain: size=" + certificateChain.size());
+    }
+}
