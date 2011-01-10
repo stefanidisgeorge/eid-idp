500f29fdb7716fd839e0c647ed47d90a9590b49b
wim.vandenhaute@gmail.com <wim.vandenhaute@gmail.com@c569a5dc-3e0d-11de-97fb-aff7d2a6cc54>
2011-01-10 11:13:39 +0000

    Cleanup no longer used WS-Federation schemas

--- trunk/eid-idp-protocol-ws-federation/src/main/java/be/fedict/eid/idp/protocol/ws_federation/AbstractWSFederationMetadataHttpServlet.java
+++ trunk/eid-idp-protocol-ws-federation/src/main/java/be/fedict/eid/idp/protocol/ws_federation/AbstractWSFederationMetadataHttpServlet.java
@@ -25,8 +25,6 @@ import be.fedict.eid.idp.spi.IdentityProviderConfiguration;
 import be.fedict.eid.idp.spi.IdentityProviderConfigurationFactory;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.xml.security.utils.Constants;
-import org.apache.xpath.XPathAPI;
 import org.opensaml.Configuration;
 import org.opensaml.DefaultBootstrap;
 import org.opensaml.saml2.metadata.EntityDescriptor;
@@ -38,9 +36,7 @@ import org.opensaml.xml.ConfigurationException;
 import org.opensaml.xml.XMLObjectBuilder;
 import org.opensaml.xml.security.credential.UsageType;
 import org.opensaml.xml.security.keyinfo.KeyInfoHelper;
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -48,27 +44,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.bind.JAXBException;
 import javax.xml.crypto.MarshalException;
-import javax.xml.crypto.dsig.*;
-import javax.xml.crypto.dsig.dom.DOMSignContext;
-import javax.xml.crypto.dsig.keyinfo.KeyInfo;
-import javax.xml.crypto.dsig.keyinfo.KeyInfoFactory;
-import javax.xml.crypto.dsig.keyinfo.X509Data;
-import javax.xml.crypto.dsig.spec.C14NMethodParameterSpec;
-import javax.xml.crypto.dsig.spec.TransformParameterSpec;
+import javax.xml.crypto.dsig.XMLSignatureException;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.*;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactoryConfigurationError;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyStore;
 import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
 import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
-import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 
@@ -88,7 +74,7 @@ public abstract class AbstractWSFederationMetadataHttpServlet extends HttpServle
         try {
             DefaultBootstrap.bootstrap();
 
-            // register WS-Trust elements needed in WS-Federation
+            // register WS-Federation Metadata elements
             Configuration.registerObjectProvider(
                     ClaimType.DEFAULT_ELEMENT_NAME,
                     new ClaimTypeBuilder(),
@@ -168,20 +154,16 @@ public abstract class AbstractWSFederationMetadataHttpServlet extends HttpServle
                 + "/protocol/" + getPath();
         LOG.debug("location: " + location);
 
-        EntityDescriptor ed = Saml2Util.buildXMLObject(EntityDescriptor.class,
+        EntityDescriptor entityDescriptor = Saml2Util.buildXMLObject(EntityDescriptor.class,
                 EntityDescriptor.DEFAULT_ELEMENT_NAME);
-        ed.setEntityID(location);
-        ed.setID("saml-metadata-" + UUID.randomUUID().toString());
+        entityDescriptor.setEntityID(location);
+        entityDescriptor.setID("saml-metadata-" + UUID.randomUUID().toString());
 
         XMLObjectBuilder<SecurityTokenService> builder =
                 Configuration.getBuilderFactory().getBuilder(SecurityTokenService.TYPE_NAME);
         SecurityTokenService securityTokenService = builder.buildObject(
                 RoleDescriptor.DEFAULT_ELEMENT_NAME, SecurityTokenService.TYPE_NAME);
-
-//        SecurityTokenService securityTokenService = Saml2Util.buildXMLObject(
-//                SecurityTokenService.class,
-//                SecurityTokenService.DEFAULT_ELEMENT_NAME);
-        ed.getRoleDescriptors().add(securityTokenService);
+        entityDescriptor.getRoleDescriptors().add(securityTokenService);
 
         securityTokenService.addSupportedProtocol("http://docs.oasis-open.org/wsfed/federation/200706");
 
@@ -292,44 +274,17 @@ public abstract class AbstractWSFederationMetadataHttpServlet extends HttpServle
         if (null != identity) {
 
             LOG.debug("sign WS-Federation Metadata");
-            element = Saml2Util.signAsElement(ed, ed,
+            element = Saml2Util.signAsElement(entityDescriptor, entityDescriptor,
                     (X509Certificate) identity.getCertificate(),
                     identity.getPrivateKey());
         } else {
 
             // TODO: explode here? will fail at RP for sure if not signed so ...
-            LOG.warn("assertion NOT signed!");
-            element = Saml2Util.marshall(ed);
+            LOG.warn("WS-Federation Metadata NOT signed!");
+            element = Saml2Util.marshall(entityDescriptor);
         }
 
-//        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         Saml2Util.writeDocument(element.getOwnerDocument(), outputStream);
-//        return new String(outputStream.toByteArray());
-
-
-//        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
-//                .newInstance();
-//        documentBuilderFactory.setNamespaceAware(true);
-//        DocumentBuilder documentBuilder = documentBuilderFactory
-//                .newDocumentBuilder();
-//        Document document = documentBuilder.newDocument();
-//
-//        JAXBContext context = JAXBContext
-//                .newInstance(
-//                        ObjectFactory.class,
-//                        org.oasis_open.docs.wsfed.federation._200706.ObjectFactory.class);
-//        Marshaller marshaller = context.createMarshaller();
-//        marshaller.setProperty("com.sun.xml.bind.namespacePrefixMapper",
-//                new WSFederationNamespacePrefixMapper());
-//        marshaller.marshal(objectFactory
-//                .createEntityDescriptor(entityDescriptor), document);
-//
-//        if (null != identity) {
-//            signDocument(document, identity.getPrivateKey(),
-//                    (X509Certificate) identity.getCertificate(), id);
-//        }
-//
-//        writeDocument(document, outputStream);
     }
 
     private void addClaimType(
@@ -355,75 +310,6 @@ public abstract class AbstractWSFederationMetadataHttpServlet extends HttpServle
                         Description.DEFAULT_ELEMENT_NAME);
         claimDescription.setValue(description);
         claim.setDescription(claimDescription);
-    }
-
-    private void signDocument(Document document, PrivateKey privateKey,
-                              X509Certificate certificate, String documentId)
-            throws TransformerException, NoSuchAlgorithmException,
-            InvalidAlgorithmParameterException, MarshalException,
-            XMLSignatureException {
-
-        Element nsElement = document.createElement("ns");
-        nsElement.setAttributeNS(Constants.NamespaceSpecNS, "xmlns:md",
-                "urn:oasis:names:tc:SAML:2.0:metadata");
-        Node roleDescriptorNode = XPathAPI.selectSingleNode(document,
-                "//md:RoleDescriptor", nsElement);
-        if (null == roleDescriptorNode) {
-            throw new IllegalStateException(
-                    "RoleDescriptor element not present");
-        }
-
-        XMLSignatureFactory signatureFactory = XMLSignatureFactory.getInstance(
-                "DOM", new org.jcp.xml.dsig.internal.dom.XMLDSigRI());
-
-        XMLSignContext signContext = new DOMSignContext(privateKey, document
-                .getDocumentElement(), roleDescriptorNode);
-        signContext.putNamespacePrefix(
-                javax.xml.crypto.dsig.XMLSignature.XMLNS, "ds");
-        DigestMethod digestMethod = signatureFactory.newDigestMethod(
-                DigestMethod.SHA1, null);
-
-        List<Transform> transforms = new LinkedList<Transform>();
-        transforms.add(signatureFactory.newTransform(Transform.ENVELOPED,
-                (TransformParameterSpec) null));
-        Transform exclusiveTransform = signatureFactory
-                .newTransform(CanonicalizationMethod.EXCLUSIVE,
-                        (TransformParameterSpec) null);
-        transforms.add(exclusiveTransform);
-
-        Reference reference = signatureFactory.newReference("#" + documentId,
-                digestMethod, transforms, null, null);
-
-        SignatureMethod signatureMethod = signatureFactory.newSignatureMethod(
-                SignatureMethod.RSA_SHA1, null);
-        CanonicalizationMethod canonicalizationMethod = signatureFactory
-                .newCanonicalizationMethod(CanonicalizationMethod.EXCLUSIVE,
-                        (C14NMethodParameterSpec) null);
-        SignedInfo signedInfo = signatureFactory.newSignedInfo(
-                canonicalizationMethod, signatureMethod, Collections
-                .singletonList(reference));
-
-        List<Object> keyInfoContent = new LinkedList<Object>();
-        KeyInfoFactory keyInfoFactory = KeyInfoFactory.getInstance();
-        List<Object> x509DataObjects = new LinkedList<Object>();
-        x509DataObjects.add(certificate);
-        X509Data x509Data = keyInfoFactory.newX509Data(x509DataObjects);
-        keyInfoContent.add(x509Data);
-        KeyInfo keyInfo = keyInfoFactory.newKeyInfo(keyInfoContent);
-
-        javax.xml.crypto.dsig.XMLSignature xmlSignature = signatureFactory
-                .newXMLSignature(signedInfo, keyInfo);
-        xmlSignature.sign(signContext);
-    }
-
-    protected void writeDocument(Document document,
-                                 OutputStream documentOutputStream)
-            throws TransformerFactoryConfigurationError, TransformerException,
-            IOException {
-        Result result = new StreamResult(documentOutputStream);
-        Transformer xformer = TransformerFactory.newInstance().newTransformer();
-        Source source = new DOMSource(document);
-        xformer.transform(source, result);
     }
 
     protected abstract String getPath();
