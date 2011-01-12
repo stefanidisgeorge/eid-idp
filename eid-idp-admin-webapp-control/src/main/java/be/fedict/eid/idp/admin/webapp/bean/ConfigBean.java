992a4428389748babf098f9f4a694a0d9e2de6ce
wim.vandenhaute <wim.vandenhaute@gmail.com>
2011-01-12 10:34:12 +0000

    Add secure channel binding

--- eid-idp-admin-webapp-control/src/main/java/be/fedict/eid/idp/admin/webapp/bean/ConfigBean.java
+++ eid-idp-admin-webapp-control/src/main/java/be/fedict/eid/idp/admin/webapp/bean/ConfigBean.java
@@ -176,6 +176,15 @@ public class ConfigBean implements Config {
                 this.log.debug("save applet config");
 
                 // Applet config
+                try {
+                        this.appletConfig.setServerCertificate(
+                                getCertificate(this.certificateBytes));
+                } catch (CertificateException e) {
+                        this.log.error("Certificate exception: " + e.getMessage(), e);
+                        this.facesMessages.addToControl("upload", "Invalid certificate");
+                        return null;
+                }
+
                 this.configuration.setAppletConfig(this.appletConfig);
 
                 this.selectedTab = ConfigurationTab.tab_applet.name();
@@ -308,13 +317,6 @@ public class ConfigBean implements Config {
                                 .getFile());
                 } else {
                         this.certificateBytes = item.getData();
-                }
-
-                try {
-                        this.appletConfig.setServerCertificate(
-                                getCertificate(this.certificateBytes));
-                } catch (CertificateException e) {
-                        this.facesMessages.addToControl("upload", "Invalid certificate");
                 }
         }
 
