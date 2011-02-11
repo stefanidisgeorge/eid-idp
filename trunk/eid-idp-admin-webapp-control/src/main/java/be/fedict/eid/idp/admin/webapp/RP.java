2d25f974986e183f6aeac45d929bd55dcd6125bb
wim.vandenhaute@gmail.com <wim.vandenhaute@gmail.com@c569a5dc-3e0d-11de-97fb-aff7d2a6cc54>
2011-02-11 12:54:12 +0000

    Admin portal: RP: Attribute PublicKey secret + certificate removable...

--- trunk/eid-idp-admin-webapp-control/src/main/java/be/fedict/eid/idp/admin/webapp/RP.java
+++ trunk/eid-idp-admin-webapp-control/src/main/java/be/fedict/eid/idp/admin/webapp/RP.java
@@ -74,6 +74,10 @@ public interface RP {
 
         String remove();
 
+        String removeAttributePublic();
+
+        String removeCertificate();
+
         String back();
 
         String selectAttributes();
