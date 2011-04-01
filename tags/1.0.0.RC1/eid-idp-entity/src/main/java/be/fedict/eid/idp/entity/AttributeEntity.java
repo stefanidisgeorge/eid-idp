a803755fbc73283e5846d661cd88ad552a1c2d4c
frank.cornelis <frank.cornelis@c569a5dc-3e0d-11de-97fb-aff7d2a6cc54>
2011-04-01 13:10:55 +0000

    tagging 1.0.0.RC1

---
+++ tags/1.0.0.RC1/eid-idp-entity/src/main/java/be/fedict/eid/idp/entity/AttributeEntity.java
@@ -0,0 +1,93 @@
+/*
+ * eID Identity Provider Project.
+ * Copyright (C) 2010 FedICT.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/.
+ */
+
+package be.fedict.eid.idp.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = Constants.DATABASE_TABLE_PREFIX + "attributes")
+@NamedQueries(@NamedQuery(name = AttributeEntity.LIST_ALL,
+        query = "FROM AttributeEntity"))
+public class AttributeEntity implements Serializable {
+
+        private static final long serialVersionUID = 1L;
+
+        public static final String LIST_ALL = "idp.attr.all";
+
+        private String uri;
+        private String name;
+        private String description;
+        private Set<AttributeProtocolUriEntity> protocolUris;
+
+        public AttributeEntity() {
+                super();
+        }
+
+        public AttributeEntity(String name, String description, String uri) {
+                this.name = name;
+                this.description = description;
+                this.uri = uri;
+        }
+
+        @Id
+        public String getUri() {
+                return this.uri;
+        }
+
+        public void setUri(String uri) {
+                this.uri = uri;
+        }
+
+        public String getName() {
+                return name;
+        }
+
+        public void setName(String name) {
+                this.name = name;
+        }
+
+        @Column(nullable = true)
+        public String getDescription() {
+                return description;
+        }
+
+        public void setDescription(String description) {
+                this.description = description;
+        }
+
+        @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.REMOVE,
+                mappedBy = AttributeProtocolUriEntity.ATTRIBUTE_COLUMN_NAME)
+        public Set<AttributeProtocolUriEntity> getProtocolUris() {
+                return this.protocolUris;
+        }
+
+        public void setProtocolUris(Set<AttributeProtocolUriEntity> protocolUris) {
+                this.protocolUris = protocolUris;
+        }
+
+        @SuppressWarnings("unchecked")
+        public static List<AttributeEntity> listAttributes(EntityManager entityManager) {
+
+                Query query = entityManager.createNamedQuery(LIST_ALL);
+                return query.getResultList();
+        }
+}
