f4748675107dc450c621253f130f4639f4b326e7
frank.cornelis <frank.cornelis@c569a5dc-3e0d-11de-97fb-aff7d2a6cc54>
2011-09-22 13:14:21 +0000

    remove buggy release

--- tags/1.0.0.RC6/eid-idp-protocol-ws-federation/src/main/java/be/fedict/eid/idp/protocol/ws_federation/wsfed/DescriptionUnmarshaller.java
+++
@@ -1,6 +0,0 @@
-package be.fedict.eid.idp.protocol.ws_federation.wsfed;
-
-import org.opensaml.xml.schema.impl.XSURIUnmarshaller;
-
-public class DescriptionUnmarshaller extends XSURIUnmarshaller {
-}
--- tags/1.0.0.RC6/eid-idp-protocol-ws-federation/src/main/java/be/fedict/eid/idp/protocol/ws_federation/wsfed/SecurityTokenServiceMarshaller.java
+++
@@ -1,6 +0,0 @@
-package be.fedict.eid.idp.protocol.ws_federation.wsfed;
-
-import org.opensaml.saml2.metadata.impl.RoleDescriptorMarshaller;
-
-public class SecurityTokenServiceMarshaller extends RoleDescriptorMarshaller {
-}
