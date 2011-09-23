5114647fc7a7383b34b2bf11d66cd5b69af43b1a
frank.cornelis <frank.cornelis@c569a5dc-3e0d-11de-97fb-aff7d2a6cc54>
2011-09-23 12:06:34 +0000

    tagging 1.0.0.RC6

---
+++ tags/1.0.0.RC6/eid-idp-protocol-ws-federation/src/main/java/be/fedict/eid/idp/protocol/ws_federation/wsfed/SecurityTokenServiceUnmarshaller.java
@@ -0,0 +1,28 @@
+package be.fedict.eid.idp.protocol.ws_federation.wsfed;
+
+import org.opensaml.saml2.metadata.impl.RoleDescriptorUnmarshaller;
+import org.opensaml.xml.XMLObject;
+import org.opensaml.xml.io.UnmarshallingException;
+
+public class SecurityTokenServiceUnmarshaller extends
+		RoleDescriptorUnmarshaller {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	protected void processChildElement(XMLObject parentSAMLObject,
+			XMLObject childSAMLObject) throws UnmarshallingException {
+
+		SecurityTokenService securityTokenService = (SecurityTokenService) parentSAMLObject;
+
+		if (childSAMLObject instanceof PassiveRequestorEndpoint) {
+			securityTokenService.getPassiveRequestorEndpoints().add(
+					(PassiveRequestorEndpoint) childSAMLObject);
+		} else if (childSAMLObject instanceof ClaimTypesOffered) {
+			securityTokenService
+					.setClaimTypesOffered((ClaimTypesOffered) childSAMLObject);
+		} else {
+			super.processChildElement(parentSAMLObject, childSAMLObject);
+		}
+	}
+}
