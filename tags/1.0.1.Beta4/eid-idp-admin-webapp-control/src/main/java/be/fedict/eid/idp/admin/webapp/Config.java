05a82a2ee06a3a7cf340f884fc669451f9d57275
frank.cornelis <frank.cornelis@c569a5dc-3e0d-11de-97fb-aff7d2a6cc54>
2012-03-12 13:29:42 +0000

    tagging 1.0.1.Beta4

---
+++ tags/1.0.1.Beta4/eid-idp-admin-webapp-control/src/main/java/be/fedict/eid/idp/admin/webapp/Config.java
@@ -0,0 +1,116 @@
+/*
+ * eID Identity Provider Project.
+ * Copyright (C) 2010 FedICT.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/.
+ */
+
+package be.fedict.eid.idp.admin.webapp;
+
+import be.fedict.eid.idp.entity.AppletConfigEntity;
+import org.richfaces.event.UploadEvent;
+
+import javax.ejb.Local;
+import javax.faces.model.SelectItem;
+import java.io.IOException;
+import java.util.List;
+
+@Local
+public interface Config {
+
+	/*
+	 * Accessors.
+	 */
+	String getXkmsUrl();
+
+	void setXkmsUrl(String xkmsUrl);
+
+	String getXkmsAuthTrustDomain();
+
+	void setXkmsAuthTrustDomain(String xkmsAuthTrustDomain);
+
+	String getXkmsIdentTrustDomain();
+
+	void setXkmsIdentTrustDomain(String xkmsIdentTrustDomain);
+
+	String getHmacSecret();
+
+	void setHmacSecret(String hmacSecret);
+
+	Boolean getHttpProxy();
+
+	void setHttpProxy(Boolean httpProxy);
+
+	String getHttpProxyHost();
+
+	void setHttpProxyHost(String httpProxyHost);
+
+	Integer getHttpProxyPort();
+
+	void setHttpProxyPort(Integer httpProxyPort);
+
+	String getIssuer();
+
+	void setIssuer(String issuer);
+
+	AppletConfigEntity getAppletConfig();
+
+	void setAppletConfig(AppletConfigEntity appletConfig);
+
+	String getSelectedTab();
+
+	void setSelectedTab(String selectedTab);
+
+	Integer getTokenValidity();
+
+	void setTokenValidity(Integer tokenValidity);
+
+	Boolean getRemoveCard();
+
+	void setRemoveCard(Boolean removeCard);
+
+	Boolean getTransactionMessageSigning();
+
+	void setTransactionMessageSigning(Boolean transactionMessageSigning);
+
+	/*
+	 * Listeners.
+	 */
+	void uploadListener(UploadEvent event) throws IOException;
+
+	/*
+	 * Factories
+	 */
+	List<SelectItem> keyStoreTypeFactory();
+
+	/*
+	 * Actions.
+	 */
+	String saveIdP();
+
+	String saveXkms();
+
+	String saveNetwork();
+
+	String saveApplet();
+
+	String removeApplet();
+
+	/*
+	 * Lifecycle.
+	 */
+	void destroy();
+
+	void postConstruct();
+}
---
+++ tags/1.0.1.Beta4/eid-idp-model/src/main/java/be/fedict/eid/idp/model/Configuration.java
@@ -0,0 +1,48 @@
+/*
+ * eID Digital Signature Service Project.
+ * Copyright (C) 2010 FedICT.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/.
+ */
+
+package be.fedict.eid.idp.model;
+
+import be.fedict.eid.idp.entity.AppletConfigEntity;
+
+import javax.ejb.Local;
+import java.util.List;
+
+@Local
+public interface Configuration {
+
+	AppletConfigEntity getAppletConfig();
+
+	void setAppletConfig(AppletConfigEntity appletConfig);
+
+	void removeAppletConfig(AppletConfigEntity appletConfig);
+
+	void setValue(ConfigProperty configProperty, String index, Object value);
+
+	void setValue(ConfigProperty configProperty, Object value);
+
+	<T> T getValue(ConfigProperty configProperty, String index, Class<T> type);
+
+	<T> T getValue(ConfigProperty configProperty, Class<T> type);
+
+	void removeValue(ConfigProperty configProperty, String index);
+
+	void removeValue(ConfigProperty configProperty);
+
+	List<String> getIndexes(ConfigProperty configProperty);
+}
