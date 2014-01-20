3586f3e552ddbdbb1159426249956e09683a97d3
frank.cornelis <frank.cornelis@c569a5dc-3e0d-11de-97fb-aff7d2a6cc54>
2014-01-20 10:00:29 +0000

    fix accoutning

--- eid-idp-model/src/main/java/be/fedict/eid/idp/model/bean/AccountingServiceBean.java
+++ eid-idp-model/src/main/java/be/fedict/eid/idp/model/bean/AccountingServiceBean.java
@@ -1,6 +1,7 @@
 /*
  * eID Identity Provider Project.
  * Copyright (C) 2010 FedICT.
+ * Copyright (C) 2014 e-Contract.be BVBA.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version
@@ -18,6 +19,8 @@
 
 package be.fedict.eid.idp.model.bean;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import javax.ejb.Stateless;
@@ -40,44 +43,52 @@ public class AccountingServiceBean implements AccountingService {
 
 	@Override
 	public List<AccountingEntity> listAll() {
-
 		LOG.debug("list all");
 		return AccountingEntity.listAll(this.entityManager);
 	}
 
 	@Override
 	public void resetAll() {
-
 		LOG.debug("reset all: #deleted="
 				+ AccountingEntity.resetAll(this.entityManager));
 	}
 
 	@Override
 	public void addRequest(String domain) {
-
 		if (null == domain) {
 			return;
 		}
 
 		LOG.debug("Add request: " + domain);
+		domain = normalize(domain);
+		LOG.debug("normalized domain: " + domain);
 
 		AccountingEntity accountingEntity = this.entityManager.find(
 				AccountingEntity.class, domain);
 		if (null == accountingEntity) {
-
 			accountingEntity = new AccountingEntity(domain);
 			this.entityManager.persist(accountingEntity);
-
 		} else {
-
 			accountingEntity.setRequests(accountingEntity.getRequests() + 1);
-
 		}
+	}
+
+	private String normalize(String domain) {
+		URI uri;
+		try {
+			uri = new URI(domain);
+		} catch (URISyntaxException e) {
+			return domain;
+		}
+		String scheme = uri.getScheme();
+		if ("http".equals(scheme) || "https".equals(scheme)) {
+			return uri.getScheme() + "://" + uri.getHost() + uri.getPath();
+		}
+		return domain;
 	}
 
 	@Override
 	public long getNumberOfRequests() {
-
 		LOG.debug("get # of requests");
 		return AccountingEntity.getNumberOfRequests(this.entityManager);
 	}
